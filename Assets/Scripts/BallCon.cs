811fa737014b3f45220a56b97b8d439979e9ae0d
Lychnus15 <nendraok@gmail.com>
2024-02-29 23:46:57 +0700

    pinball
    Setup Project Game Pinball3D

---
+++ Assets/Scripts/BallCon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCon : MonoBehaviour
+{
+    public float maxSpeed;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidbody.velocity.magnitude > maxSpeed)
+        {
+            rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Padle_Con.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Padle : MonoBehaviour
+{
+    public KeyCode input;
+    public float targetPressed;
+    public float targetReleased;
+    private HingeJoint hinge;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+
+        targetPressed = hinge.limits.max;
+        targetReleased = hinge.limits.min;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ReadInput();
+    }
+
+    private void ReadInput()
+    {
+        JointSpring jointSpring = hinge.spring;
+        if (Input.GetKey(input))
+        {
+            jointSpring.targetPosition = targetPressed;
+        }
+        else
+        {
+            jointSpring.targetPosition = targetReleased;
+        }
+
+        hinge.spring = jointSpring;
+    }
+}
